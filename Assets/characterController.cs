77125d7addea4f2c76bd8623381fe01d470f98d8
Aurélien <aurelien1999@msn.com>
2020-02-22 18:40:26 +0100

    [+] Helicopter Game

---
+++ Assets/characterController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float current_speed; 
+    
+    public Transform topCheck;
+    public Transform botCheck;
+    public Transform leftCheck;
+    public Transform rightCheck;
+    public LayerMask whatIsObject;
+    public LayerMask whatIsWin;
+    public float jumpForce = 4f;
+    public float speed = 4f;
+    public float speedIncrement = 0.1f;
+    public GameObject restart;
+    public GameObject win;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        current_speed = speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3(1f, 0f, 0f);
+        transform.position += movement * Time.deltaTime * current_speed;
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            current_speed += speedIncrement;
+        }
+        if (Input.GetKey(KeyCode.R))
+        {
+            current_speed = speed;
+            transform.position = new Vector3(0, 0, 0);
+            Time.timeScale = 1f;
+            restart.SetActive(false);
+        }
+        if (Physics2D.OverlapCircle(botCheck.position, 0f, whatIsObject) || Physics2D.OverlapCircle(topCheck.position, 0f, whatIsObject) || Physics2D.OverlapCircle(rightCheck.position, 0f, whatIsObject) || Physics2D.OverlapCircle(leftCheck.position, 0f, whatIsObject))
+        {
+            restart.SetActive(true);
+            Time.timeScale = 0f;
+        }
+        if (Physics2D.OverlapCircle(botCheck.position, 0f, whatIsWin) || Physics2D.OverlapCircle(topCheck.position, 0f, whatIsWin) || Physics2D.OverlapCircle(rightCheck.position, 0f, whatIsWin) || Physics2D.OverlapCircle(leftCheck.position, 0f, whatIsWin))
+        {
+            win.SetActive(true);
+            Time.timeScale = 0f;
+        }
+    }
+}
